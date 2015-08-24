0daf20f54d1925ad31af851bf26fd4820dd7c7bb
Les Ansley <les@ansleyfamily.com>
2015-08-24 15:33:07 +0100

    added grunt module inline-css

---
+++ grunt/inline-css.js
@@ -0,0 +1,10 @@
+module.exports = {
+    main: {
+        options: {
+        },
+        files: {
+            'src/index.html': 'dist/index.html',
+            'src/pizza.html': 'dist/pizza.html'
+        }
+    }
+};
